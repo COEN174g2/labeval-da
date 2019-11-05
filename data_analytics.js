27a53e4da0df6051c605b10cb48c94fb1249426b
Xinyu Chen <49008979+Serendipitysc@users.noreply.github.com>
2019-11-04 17:15:44 -0800

    fix the csv parsing issue on data_analytic.js

--- data_analytics.js
+++ data_analytics.js
@@ -11,21 +11,21 @@ function loadInfo(number){
 		img2.src = "polarity_chart_question_" + number + ".png";
 		document.body.appendChild(img);
 		document.body.appendChild(img2);
-		text.innerHTML = "percentage positive response:"+ question_info[number-1][1] 
-						+"\npercentage negative response:"+ question_info[number-1][2] 
-						+"\npercentage subjective:"+ question_info[number-1][3] 
-						+"\npercentage objective:"+ question_info[number-1][4] 
-						+ "\npercentage netural:"+ question_info[number-1][5]; 
+		text.innerHTML = "percentage positive response:"+ question_info[number-1][2] 
+						+"\npercentage negative response:"+ question_info[number-1][3] 
+						+"\npercentage subjective:"+ question_info[number-1][4] 
+						+"\npercentage objective:"+ question_info[number-1][5] 
+						+ "\npercentage netural:"+ question_info[number-1][6]; 
 		return;
 	}
 	img2.src = "";
 	img.src="histogram_question_" + number + ".png";
 	document.body.appendChild(img);
-	text.innerHTML = " the most popular choice:" + question_info[number-1][1] 
-					+ "\nmean:"+ question_info[number-1][2]
-					+"\nmode:"+ question_info[number-1][3] 
-					+"\nmedian:"+question_info[number-1][4]
-					+"\nstandard_deviation:"+question_info[number-1][5];
+	text.innerHTML = " the most popular choice:" + question_info[number-1][2] 
+					+ "\nmean:"+ question_info[number-1][3]
+					+"\nmode:"+ question_info[number-1][4] 
+					+"\nmedian:"+question_info[number-1][5]
+					+"\nstandard_deviation:"+question_info[number-1][6];
 }
 var createClickHandler = function(arg) {
   return function() { loadInfo(arg); };
@@ -44,26 +44,6 @@ function create_entry(){
 
 }
 
-/*function readTextFile(file)
-{
-    var rawFile = new XMLHttpRequest();
-    rawFile.open("GET", file, true);
-    rawFile.onreadystatechange = function ()
-    {
-        if(rawFile.readyState === 4)
-        {
-            if(rawFile.status === 200 || rawFile.status == 0)
-            {
-                var allText = rawFile.responseText;
-                console.log(allText);
-            }
-        }
-    }
-    rawFile.send(null);
-}
-
-
-readTextFile("analytics.csv")*/
 
 $.ajaxPrefilter( 'text', function( options ) {
     options.crossDomain = true;
@@ -75,7 +55,7 @@ $.ajax({
     dataType: "text",
     success: function(data) {
         //alert("worked");
-   		parse_data(data);
+   		parse(data);
     },
     error: function (request, status, error) {
 
@@ -92,10 +72,33 @@ function parse_data(allText){
 		var entry = allTextLines[i]
 		if (entry.length!=0){
 			console.log(i);
+
 			var instance = entry.split(',');
 			if (instance[0] == "multiple_choice" || instance[0] == "open_ended")
 			{
 				question_info.push(instance);
+			}
+		}
+	}
+	var j;
+	for (j=0;j<question_info.length;j++)
+	{
+		console.log(question_info[j]);
+	}
+}
+
+function parse(allText){
+	var question_number=1;
+	var allTextLines = Papa.parse(allText).data;
+	var i;
+	for (i=0; i<allTextLines.length; i++){
+		var entry = allTextLines[i]
+		if (entry.length!=1){
+			console.log(i);
+			console.log("this is the entry:",entry,"the length of entry is:", entry.length);
+			if (entry[0] == "multiple_choice" || entry[0] == "open_ended")
+			{
+				question_info.push(entry);
 			}
 		}
 	}
